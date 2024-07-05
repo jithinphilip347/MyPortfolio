d954cde42c36c2f61d13d3a096537b07b758d50a
jithin philip <jithinphilip347@gmail.com>
2024-07-05 22:35:52 +0530

    completed mobile navbar

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -27,7 +27,7 @@ const links = [
     name: 'contact',
     path: '/contact',
  }, 
-]
+];
 
 const MobileNav = () => {
 const pathname = usePathname();
@@ -44,17 +44,18 @@ const pathname = usePathname();
             </h1>
             </Link>
             </div>
-          <nav className='flex flex-col justify-center gap-8'>
+          <nav className='flex flex-col justify-center items-center gap-8'>
             {links.map((link, index) => {
                 return (
-                <Link 
-                href={link.path} 
-                key={index} 
-                className={`${
-                link.path === pathname && "text-accent border-b-2 border-accent"
-                 }text-xl capitalize hover:text-accent transition-all`}>
-                {link.name}
-                 </Link>
+                    <Link 
+                    href={link.path} 
+                    key={index} 
+                    className={`${
+                        link.path === pathname &&
+                        "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}
+                        >
+                       {link.name}
+                        </Link>
                 );
             })}
           </nav>
